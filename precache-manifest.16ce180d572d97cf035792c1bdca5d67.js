c8a06fd15360470a5f6df67fe9fc37d262fcc1c6
reniejoy <rjfuncion@gmail.com>
2020-07-19 12:41:42 +0800

    Updates

---
+++ precache-manifest.16ce180d572d97cf035792c1bdca5d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8f536bbb6808eb3adbed39ced8e639",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "cbe69060690d57ea9ce9",
+    "url": "/demo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92f7981eeb8ed1beb6d5",
+    "url": "/demo-app/static/js/2.395e7c86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-app/static/js/2.395e7c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe69060690d57ea9ce9",
+    "url": "/demo-app/static/js/main.94643073.chunk.js"
+  },
+  {
+    "revision": "c04c7ade94de033a4955",
+    "url": "/demo-app/static/js/runtime-main.f284e742.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
